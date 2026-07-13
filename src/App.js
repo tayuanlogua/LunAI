934f52ca29d210cea492e730584e4cb662f6caed
Talia Lopez <tayuanlogua.12@gmail.com>
2026-07-13 16:39:11 -0500

    Cambios en backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const iniciarSesion = () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+      alert("Completa los campos");
+      return;
+    }
+
+    if (email === "admin@lunai.com" && password === "123456") {
+      alert("Bienvenida a lunAI 🌙✨");
+    } else {
+      alert("Datos incorrectos");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={require('./imagenes/logo.png')} alt="Logo lunAI" className="logo-img" />
+      <p className="subtitle">Conecta con tu ciclo. Transforma tu vida.</p>
+
+      <div className="card">
+        <div className="logo">🌙</div>
+
+        <input id="email" type="email" placeholder="Correo electrónico" />
+        <input id="password" type="password" placeholder="Contraseña" />
+
+        <div className="links">
+          <a href="#">¿Olvidaste tu contraseña?</a>
+          <a href="#">Crear cuenta</a>
+        </div>
+
+        <button onClick={iniciarSesion}>Iniciar sesión</button>
+      </div>
     </div>
   );
 }
